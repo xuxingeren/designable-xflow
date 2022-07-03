8085bbf29f4ce189ced0ae18d9c2b67ce4bee10d
xuxin <908642754@qq.com>
2022-07-03 23:37:33 +0800

    feat: xflow init

---
+++ config/config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  layout: {},
+  routes: [
+    {
+      path: '/',
+      redirect: '/xflow',
+    },
+    {
+      name: '流程可视化',
+      path: '/xflow',
+      icon: 'SmileOutlined',
+      component: '@/pages/Xflow',
+    },
+  ],
+  mfsu: {},
+  fastRefresh: {},
+});
